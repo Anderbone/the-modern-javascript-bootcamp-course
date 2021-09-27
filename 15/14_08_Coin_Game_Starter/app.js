5fad04a16fff9bb4f02b660ee6e355e160546be7
Anderbone <yanchiyu@outlook.com>
2021-09-27 22:17:50 +0100

    !

--- 15/14_08_Coin_Game_Starter/app.js
+++ 15/14_08_Coin_Game_Starter/app.js
@@ -9,3 +9,37 @@ function isTouching(a, b) {
 		aRect.left > bRect.left + bRect.width
 	);
 }
+
+const player = document.querySelector('#player');
+const coin = document.querySelector('#coin')
+
+function randomCoin(){
+	coin.style.left = Math.random() * window.innerWidth + 'px';
+	coin.style.top = Math.random() * window.innerHeight + 'px';
+}
+
+window.addEventListener('keyup', function(e) {
+	if (e.key === 'ArrowDown') {
+		player.style.top = extractPos(player.style.top) + 50 + 'px'
+	}
+	else if (e.key === 'ArrowUp') {
+		player.style.top = extractPos(player.style.top) - 50 + 'px'
+	}
+	else if (e.key === 'ArrowLeft') {
+		player.style.left = extractPos(player.style.left) - 50 + 'px'
+	}
+	else if (e.key === 'ArrowRight') {
+		console.log(player.style.right)
+		player.style.left = extractPos(player.style.left) + 50 + 'px'
+	}
+
+	if (isTouching(coin, player)){
+		console.log("touched?")
+		randomCoin();
+	}
+})
+
+const extractPos = (pos) => {
+	if (!pos) {return 0}
+	return parseInt(pos.slice(0, -2))
+}
