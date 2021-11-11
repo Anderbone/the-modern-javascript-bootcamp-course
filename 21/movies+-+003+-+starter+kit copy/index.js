4968a943289fbc8f7f16aff2b5ffff73cf52e605
Anderbone <yanchiyu@outlook.com>
2021-11-11 20:25:12 +0000

    click to remove

--- 21/movies+-+003+-+starter+kit copy/index.js
+++ 21/movies+-+003+-+starter+kit copy/index.js
@@ -31,12 +31,19 @@ const resultsWrapper = document.querySelector('.results');
 
 const onInput = async event => {
 	const movies = await fetchData(event.target.value);
+
+	if (!movies.length) {
+		dropdown.classList.remove('is-active');
+		return
+	}
+	resultsWrapper.innerHTML = '';
 	dropdown.classList.add('is-active');
 	for (let movie of movies) {
 		const option = document.createElement('a');
+		const imgSrc = movie.Poster == 'N/A' ? '' : movie.Poster;
 		option.classList.add('dropdown-item')
 		option.innerHTML = `
-		<img src="${movie.Poster}"/>
+		<img src="${imgSrc}"/>
 		${movie.Title}
 		`;
 		resultsWrapper.appendChild(option)
@@ -44,3 +51,9 @@ const onInput = async event => {
 };
 
 input.addEventListener('input', debounce(onInput, 500));
+
+document.addEventListener('click', event => {
+	if (!root.contains(event.target)){
+		dropdown.classList.remove('is-active');
+	}
+})
